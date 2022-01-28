2018ced46f91ed85055a3b6518a95ad2f1faaca4
Jonathan Mason <68861817+jonathan-timothy-mason@users.noreply.github.com>
2022-01-28 23:46:15 +0000

    Recycler view better and ID now displayed.

--- app/src/main/java/jonathan/mason/protomovies/IdentifierActivity.kt
+++ app/src/main/java/jonathan/mason/protomovies/IdentifierActivity.kt
@@ -1,12 +1,28 @@
 package jonathan.mason.protomovies
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import jonathan.mason.protomovies.api.EXTRA_MOVIE_ID
 
 class IdentifierActivity : AppCompatActivity() {
+
+    private lateinit var id: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_identifier)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         this.setTitle(R.string.identifier_screen_caption)
+
+        // Display ID passed is intent.
+        id = this.findViewById(R.id.id_text)
+        if (this.intent.hasExtra(EXTRA_MOVIE_ID)) {
+            val n = this.intent.getIntExtra(EXTRA_MOVIE_ID, -1)
+            if(n != -1) {
+                id.text = n.toString()
+            }
+
+        }
     }
 }
